9b99c4b976cc5c9326e6cd821fd2f130f965fc4c
Wiktor Ilczuk <wiktor.ilczuk2006@gmail.com>
2025-02-22 15:12:16 +0100

    the class

---
+++ app/src/main/java/com/example/myapplication/User.java
@@ -0,0 +1,53 @@
+package com.example.myapplication;
+
+public class User {
+    private String firstName;
+    private String lastName;
+    private String login;
+    private String password;
+    private Boolean isAdmin;
+
+    public User(String firstName, String lastName, String login, String password, Boolean isAdmin){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        if(login.isEmpty()){
+            this.login = lastName.substring(0, 3) + firstName.substring(0, 3);
+        }
+        else {
+            this.login = login;
+        }
+        this.password = password;
+        this.isAdmin = isAdmin;
+    }
+
+    public String GetFirstName(){
+        return firstName;
+    }
+    public String GetLastName(){
+        return lastName;
+    }
+    public String GetLogin(){
+        return login;
+    }
+    public String GetPassword(){
+        return password;
+    }
+    public Boolean GetIsAdmin(){
+        return isAdmin;
+    }
+    public void SetFirstName(String firstName){
+        this.firstName = firstName.trim();
+    }
+    public void SetLastName(String lastName){
+        this.lastName = lastName.trim();
+    }
+    public void SetLogin(String login){
+        this.login = login.trim();
+    }
+    public void SetPassword(String password){
+        this.password = password;
+    }
+    public void SetIsAdmin(Boolean isAdmin){
+        this.isAdmin = isAdmin;
+    }
+}
